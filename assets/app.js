1113e3d69bb5745be24c46b1e82aae1023756183
Yordi <yoriyordina7@gmail.com>
2023-01-10 17:26:57 +0300

    push

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 
-const book = [];
+const book = JSON.parse(localStorage.getItem('our-books'))||[];
 
 const inputTitle = document.querySelector("#text");
 const inputAuthor = document.querySelector("#txt");
@@ -9,6 +9,26 @@ const list = document.getElementById("book-list");
 const books = document.querySelector('.books');
 let i = 0;
 
+function disp(){
+    list.innerHTML = ''
+    book.map((item) => (
+        list.innerHTML += `
+        <li id="bok">${item.title} by ${item.author} <br> 
+        <button type="button" class="btn btn-outline-primary delete-btn" id="btns-${i++}">
+                        Remove</button> </li> <hr>
+        `
+        ));
+        const buttonItem = document.querySelectorAll('.delete-btn')
+        buttonItem.forEach((item) => {
+            item.addEventListener('click', (e) => {
+                const buttonElem = document.getElementById('book-list')
+                const btnEle = document.getElementById(item.id)
+                buttonElem.removeChild(btnEle.parentElement)
+                console.log
+
+            })
+        }) 
+}
 
 myButton.addEventListener("click", (e)=>{
     if(inputTitle.value && inputAuthor.value != ""){
@@ -18,28 +38,15 @@ myButton.addEventListener("click", (e)=>{
             author: inputAuthor.value,
         }
         book.push(bookData)
-        list.innerHTML = ''
-        book.map((item) => (
-            list.innerHTML += `
-            <li id="bok">${item.title} by ${item.author}  
-            <button type="button" class="btn btn-outline-primary delete-btn" id="btns-${i++}">
-            Remove</button></li> 
-            `
-            ));
+        localStorage.setItem('our-books',JSON.stringify(book))
+       
+        disp();
     
         document.querySelector('form').reset()  
-
-        const buttonItem = document.querySelectorAll('.delete-btn')
-        buttonItem.forEach((item) => {
-            item.addEventListener('click', (e) => {
-                const buttonElem = document.getElementById('book-list')
-                const btnEle = document.getElementById(item.id)
-                buttonElem.removeChild(btnEle.parentElement)
-            })
-        }) 
        
     }
 
 });
 
 
+disp();
