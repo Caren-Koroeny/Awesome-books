a6e7de08a526067f31857879818796211438e779
Caren-Koroeny <carensiya5@gmail.com>
2023-01-10 14:32:30 +0300

    solved remove error

--- assets/app.js
+++ assets/app.js
@@ -5,42 +5,41 @@ const book = [];
 const inputTitle = document.querySelector("#text");
 const inputAuthor = document.querySelector("#txt");
 const myButton = document.querySelector(".btn-list");
-const list = document.getElementById("container");
+const list = document.getElementById("book-list");
 const books = document.querySelector('.books');
 let i = 0;
+
 
 myButton.addEventListener("click", (e)=>{
     if(inputTitle.value && inputAuthor.value != ""){
         e.preventDefault();
         const bookData = {
             title: inputTitle.value,
-            author: inputAuthor.value
+            author: inputAuthor.value,
         }
         book.push(bookData)
         list.innerHTML = ''
         book.map((item) => (
             list.innerHTML += `
-            <li id="bok">${item.title} by ${item.author}  <button type="button" class="btn btn-outline-primary" id="btns-${i++}">Remove</button></li> 
+            <li id="bok">${item.title} by ${item.author}  
+            <button type="button" class="btn btn-outline-primary delete-btn" id="btns-${i++}">
+            Remove</button></li> 
             `
             ));
     
-            
-
         document.querySelector('form').reset()  
-        
-        document.querySelector("#btns-" + i)
-        .addEventListener("click",function(){
-            const remove1 = this.parentNode;
-            list.removeChild(remove1);
 
-            
-        })
-
-       
+        const buttonItem = document.querySelectorAll('.delete-btn')
+        buttonItem.forEach((item) => {
+            item.addEventListener('click', (e) => {
+                const buttonElem = document.getElementById('book-list')
+                const btnEle = document.getElementById(item.id)
+                buttonElem.removeChild(btnEle.parentElement)
+            })
+        }) 
        
     }
-   
-    
-   
 
 });
+
+
